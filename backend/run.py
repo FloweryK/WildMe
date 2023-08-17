e0e37cf1f9b5dbc459094713c3091052a4097315
FloweryK <flowerk94@gmail.com>
2023-08-18 02:27:57 +0900

    [feat] added env for server host, port

--- backend/run.py
+++ backend/run.py
@@ -15,12 +15,6 @@ if __name__ == '__main__':
     # load .env
     load_dotenv()
 
-    # parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--host', type=str, default='localhost')
-    parser.add_argument('-p', '--port', type=int, default=8080)
-    args = parser.parse_args()
-
     # app settings
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
@@ -33,7 +27,7 @@ if __name__ == '__main__':
     app.register_blueprint(InferenceBluePrint('inference', __name__), url_prefix='/inference')
 
     # run app
-    t = threading.Thread(target=app.run, kwargs={'host': args.host, 'port': args.port})
+    t = threading.Thread(target=app.run, kwargs={'host': os.environ.get('HOST'), 'port': os.environ.get('PORT')})
     t.start()
     
     # run checker
