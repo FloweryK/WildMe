d8deb2d8bd2dc974b91cecb0ebc0e5e806db18e6
FloweryK <flowerk94@gmail.com>
2023-08-22 13:11:22 +0900

    [bugfix] fixed login api interface

--- frontend/src/api/login/interface.ts
+++ frontend/src/api/login/interface.ts
@@ -10,11 +10,4 @@ export interface SignInResponse {
 export interface SignUpResponse {
   id: number;
   name: string;
-  path_coning: string | null;
-  path_data: string | null;
-  path_vocab: string | null;
-  path_weight: string | null;
-  reserve_status: string | null;
-  reserve_timestamp: number | null;
-  speaker: string | null;
 }
