e248644cd07ad1a79d6dde412c6f1044736f20cc
FloweryK <flowerk94@gmail.com>
2023-08-30 00:48:03 +0900

    [feat] refactored header, changed start and end icons' position

--- frontend/src/common/Header/index.tsx
+++ frontend/src/common/Header/index.tsx
@@ -1,27 +1,52 @@
 import { observer } from "mobx-react";
-import { AppBar, Box, Container, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, Container, Grid, IconButton } from "@mui/material";
 import Logo from "common/Logo";
 import { HeaderProps } from "./interface";
+import { styled } from "styled-components";
+
+const StyledHeader = styled.div`
+  .headerGrid {
+    display: flex;
+    align-items: center;
+  }
+  .left {
+    justify-content: flex-start;
+  }
+  .center {
+    justify-content: center;
+  }
+  .right {
+    justify-content: flex-end;
+  }
+`;
 
 const Header = (props: HeaderProps) => {
   const { startIcon, endIcon, onClickStartIcon, onClickEndIcon } = props;
 
   return (
-    <Box sx={{ flexGrow: 1, marginBottom: 8, zIndex: 1000 }}>
-      <AppBar position="fixed">
-        <Container maxWidth="xs">
-          <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
-            <IconButton color="inherit" onClick={onClickStartIcon}>
-              {startIcon}
-            </IconButton>
-            <Logo />
-            <IconButton color="inherit" onClick={onClickEndIcon}>
-              {endIcon}
-            </IconButton>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+    <StyledHeader>
+      <Box sx={{ flexGrow: 1, marginBottom: 8, zIndex: 1000 }}>
+        <AppBar position="fixed">
+          <Container maxWidth="xs">
+            <Grid container>
+              <Grid item xs={4} className="headerGrid left">
+                <IconButton color="inherit" onClick={onClickStartIcon}>
+                  {startIcon}
+                </IconButton>
+              </Grid>
+              <Grid item xs={4} className="headerGrid center">
+                <Logo />
+              </Grid>
+              <Grid item xs={4} className="headerGrid right">
+                <IconButton color="inherit" onClick={onClickEndIcon}>
+                  {endIcon}
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Container>
+        </AppBar>
+      </Box>
+    </StyledHeader>
   );
 };
 
