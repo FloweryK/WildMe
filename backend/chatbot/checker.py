[bugfix] fixed data leakaged in augmentation

backend/chatbot/checker.py
@@ -1,7 +1,7 @@
 import os
 import time
 import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import Subset, DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from chatbot.constants.custom_tokens import *
 from chatbot.dataset.base import collate_fn
@@ -11,6 +11,7 @@ from chatbot.utils import WarmupScheduler
 from chatbot.config import Config
 from chatbot.trainer import Trainer
 from db.database import db_schedule
+from pprint import pprint
 
 
 class Checker:
@@ -81,6 +82,13 @@ class Checker:
         train_size = int(config.r_split * len(dataset))
         trainset, testset = random_split(dataset, [train_size, len(dataset) - train_size])
 
+        # filter out augmented data in the testset
+        print("before filtering:", len(testset))
+        testset = Subset(testset, indices=[i for i, data in enumerate(testset) if not data['is_augmented']])
+        print("after filtering:", len(testset))
+        print("trainset:", len(trainset))
+        print("testset:", len(testset))
+
         # dataloader
         trainloader = DataLoader(trainset, batch_size=config.n_batch, shuffle=True, collate_fn=collate_fn)
         testloader = DataLoader(testset, batch_size=config.n_batch, shuffle=True, collate_fn=collate_fn)
@@ -104,7 +112,6 @@ class Checker:
         # trainer
         trainer = Trainer(model, criterion, scaler, optimizer, scheduler, writer, path_weight)
 
-        print("dataset size:", len(dataset))
         # train
         t_start = time.time()
         for epoch in range(config.n_epoch):

backend/chatbot/dataset/base.py
@@ -77,6 +77,7 @@ class ChatDatasetBase(Dataset):
             
             self.data[chat_id]['text_encode'] = text_encode
             self.data[chat_id]['text_words'] = [self.vocab.DecodeIds(tid) for tid in text_encode]
+            self.data[chat_id]['is_augmented'] = False
     
     def augment_data(self, augment_topn, augment_threshold):
         # collect tagged data
@@ -104,6 +105,7 @@ class ChatDatasetBase(Dataset):
                 neighbor = self.data[chat_id]
                 neighbor['id'] = id_cur
                 neighbor['text'] = self.data[neighbor_id]['text']
+                neighbor['is_augmented'] = True
 
                 data_augmented[id_cur] = neighbor
                 id_cur += 1
@@ -123,14 +125,19 @@ class ChatDatasetBase(Dataset):
         question_id = answer['question_id']
         question = self.data[question_id]
 
-        return (
-            torch.tensor(question['text_encode']),
-            torch.tensor(answer['text_encode']),
-        )
+        return {
+            'data': (torch.tensor(question['text_encode']), torch.tensor(answer['text_encode'])),
+            'is_augmented': answer['is_augmented']
+        }
 
 
 def collate_fn(inputs):
-    x_enc, x_dec = list(zip(*inputs))
+    x_enc = []
+    x_dec = []
+
+    for data in inputs:
+        x_enc.append(data['data'][0])
+        x_dec.append(data['data'][1])
 
     x_enc = torch.nn.utils.rnn.pad_sequence(x_enc, batch_first=True, padding_value=PAD)
     x_dec = torch.nn.utils.rnn.pad_sequence(x_dec, batch_first=True, padding_value=PAD)